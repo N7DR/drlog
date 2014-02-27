7a4d39290ef57c44ca515f9135573c62597b68e9
N7DR <N7DR@arrl.net>
2014-02-26 17:32:01 -0700

    Add drlog_error.h

---
+++ include/drlog_error.h
@@ -0,0 +1,47 @@
+// $Id: drlog_error.h 3 2012-12-30 00:02:28Z  $
+
+#ifndef DRLOG_ERROR_H
+#define DRLOG_ERROR_H
+
+/*! \file drlog_error.h
+
+    A simple base error class
+*/
+
+#include <exception>
+#include <string>
+
+/*! \class  drlog_error
+  \brief  Trivial base class for drlog errors 
+*/
+class drlog_error : public std::exception
+{
+protected:
+  int         _code;      ///< Error code
+  std::string _reason;      ///< Error reason
+
+public:
+
+/*! \brief  Constructor from code and reason
+  \param  n Error code
+  \param  s Reason
+*/
+  drlog_error(const int n, const std::string& s);
+
+/*! \brief  Destructor
+*/
+  virtual ~drlog_error(void) throw()
+    { }
+
+/*! \brief  RO access to _code
+*/
+  inline const int code(void) const
+    { return _code; }
+
+/*! \brief  RO access to _reason
+*/
+  inline const std::string reason(void) const
+    { return _reason; }
+};
+
+#endif    // DRLOG_ERROR_H
