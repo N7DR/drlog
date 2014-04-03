0ee28440ac176fe0947f10e8f162fedac252d4fd
N7DR <N7DR@arrl.net>
2014-04-03 14:47:39 -0600

    Add x_error.h

---
+++ include/x_error.h
@@ -0,0 +1,48 @@
+// $Id: x_error.h 3 2012-12-30 00:02:28Z  $
+
+#ifndef X_ERROR_H
+#define X_ERROR_H
+
+/*! \file x_error.h
+
+    A simple base error class
+*/
+
+#include <exception>
+#include <string>
+
+/*! \class  x_error
+    \brief  Trivial base class for errors
+*/
+class x_error : public std::exception
+{
+protected:
+  int         _code;            ///< Error code
+  std::string _reason;          ///< Error reason
+
+public:
+
+/*! \brief  Constructor from code and reason
+    \param  n   Error code
+    \param  s   Reason
+*/
+  x_error(const int n, const std::string& s);
+
+/*! \brief  Destructor
+*/
+  virtual ~x_error(void) throw()
+    { }
+
+/*! \brief  RO access to _code
+*/
+  inline const int code(void) const
+    { return _code; }
+
+/*! \brief  RO access to _reason
+*/
+  inline const std::string reason(void) const
+    { return _reason; }
+};
+
+#endif    // X_ERROR_H
+
